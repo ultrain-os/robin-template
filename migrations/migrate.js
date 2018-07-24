d0abf347ed179477136fb872d363f1afa10bd73c
刘飞飞 <liuff@liufeifeideMacBook-Pro.local>
2018-07-24 11:49:26 +0800

    修改部署依赖

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -1,18 +1,16 @@
-const utr = require('ultrain-jssdk');
+const utr = require('u3.js');
 const fs = require('fs');
 const path = require('path');
 const config = require('../config');
 
-const ultrain = utr.Ultrain(config);
 let deploy = async (contract, account = 'ultrainio') => {
-
+  const ultrain = utr.Ultrain(config);
   const wasm = fs.readFileSync(path.resolve(__dirname, `../build/${contract}.wast`));
   const abi = fs.readFileSync(path.resolve(__dirname, `../build/${contract}.abi`));
 
   ultrain.setcode(account, 0, 0, wasm);
   ultrain.setabi(account, JSON.parse(abi));
 
-  const code = await ultrain.getCode(account);
-  console.log(code);
+  await ultrain.getCode(account);
 };
-deploy('MyContract', 'ultrainio');
+deploy('MyContract', 'utrio.token');
