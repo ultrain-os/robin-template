6112ccc9a3fbb1732817dfab15d78e457821347e
刘飞飞 <liuff@liufeifeideMacBook-Pro.local>
2018-07-27 10:07:55 +0800

    增加abi2json

--- migrations/migrate.js
+++ migrations/migrate.js
@@ -1,6 +1,7 @@
 const utr = require('u3.js');
 const config = require('../config');
 
+u3.Ultrain.abi2json('MyContract','ultrainio');
 const ultrain = utr.Ultrain(config);
 
 ultrain.deploy('MyContract', 'utrio.token');
