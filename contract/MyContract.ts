634705695b1ac6d8e323ddd3624c1f8ad9320273
fanliangqin <fanliangqin@ultrain.io>
2018-10-02 12:43:50 +0800

    update samples

--- contract/MyContract.ts
+++ contract/MyContract.ts
@@ -1,13 +1,13 @@
 import { Contract } from 'ultrain-ts-lib/src/contract';
 import { Log } from 'ultrain-ts-lib/src/log';
-import { RN, N } from 'ultrain-ts-lib/lib/name';
+import { NAME, RNAME} from 'ultrain-ts-lib/src/account';
 import { EventObject, emit } from 'ultrain-ts-lib/src/events';
 
 class MyContract extends Contract {
 
   @action
-  hi(name: u64, age: u32, msg: string): void {
-    Log.s('hi: name = ').s(RN(name)).s(' age = ').i(age, 10).s(' msg = ').s(msg).flush();
-    emit('onHiInvoked', EventObject.setString('name', RN(name)));
+  hi(name: account_name, age: u32, msg: string): void {
+    Log.s('hi: name = ').s(RNAME(name)).s(' age = ').i(age, 10).s(' msg = ').s(msg).flush();
+    emit('onHiInvoked', EventObject.setString('name', RNAME(name)));
   }
 }
