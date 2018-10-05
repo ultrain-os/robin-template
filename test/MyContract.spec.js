05517d4eeb3a9031f1f92c8a21b115fa482c6d55
benyasin <rgwapkk520>
2018-10-05 17:56:55 +0800

    event listener update

--- test/MyContract.spec.js
+++ test/MyContract.spec.js
@@ -1,5 +1,5 @@
 const U3Utils = require('u3-utils/dist/es5');
-const { createU3, format } = require('u3.js/src');
+const { createU3, format, listener } = require('u3.js/src');
 const config = require('../config');
 
 const chai = require('chai');
@@ -11,16 +11,54 @@ const should = chai.should();
 const expect = chai.expect;
 const assert = chai.assert;
 
-describe('Contract\'s test cases', function() {
+describe('Test cases', function() {
 
-  it('case1', async () => {
+  it('event register', async () => {
 
-    let account = "ben";
+    let account = 'ben';
+    const u3 = createU3(config);
+    await u3.registerEvent(account, 'http://10.10.10.114:3002');
+
+    U3Utils.wait(1000);
+
+    listener(function(data) {
+      console.log(data);
+    });
+
+  });
+
+
+  it('event unregister', async () => {
+
+    let account = 'ben';
+    const u3 = createU3(config);
+    await u3.unregisterEvent(account, 'http://10.10.10.114:3002');
+
+    U3Utils.wait(1000);
+
+    listener(function(data) {
+      console.log(data);
+    });
+  });
+
+
+  it('call a action(not wait packed)', async () => {
+
+    let account = 'ben';
     const u3 = createU3(config);
     const contract = await u3.contract(account);
-    const tx = contract.hi('ben',30,'It is a test', { authorization: [`ben@active`] });
+    const tx = contract.hi('ben', 30, 'It is a test', { authorization: [`ben@active`] });
+  });
 
 
+  it('call a action(wait util it was packed', async () => {
+
+    let account = 'ben';
+    const u3 = createU3(config);
+    const contract = await u3.contract(account);
+    const tx = contract.hi('ben', 30, 'It is a test', { authorization: [`ben@active`] });
+
+    //wait util it was packed in a block
     let tx_trace = await u3.getTxByTxId(tx.transaction_id);
     while (!tx_trace.irreversible) {
       await U3Utils.wait(1000);
