1fdd39485b04cfa2c62e3a4e6ee4ac063414ebb2
benyasin <rgwapkk520>
2019-02-26 19:38:35 +0800

    wait for default 60s

--- test/MyContract.spec.js
+++ test/MyContract.spec.js
@@ -1,42 +1,50 @@
-const U3Utils = require('u3-utils/src');
-const { createU3, format, listener } = require('u3.js/src');
-const config = require('../config');
+const U3Utils = require("u3-utils/src");
+const { createU3, format, listener } = require("u3.js/src");
+const config = require("../config");
 
-const chai = require('chai');
-require('chai')
-  .use(require('chai-as-promised'))
+const chai = require("chai");
+require("chai")
+  .use(require("chai-as-promised"))
   .should();
 
 const should = chai.should();
 
-describe('Test cases', function() {
+describe("Test cases", function() {
 
-  it('transaction', async () => {
+  it("transaction", async () => {
 
-    let account = 'ben';
+    let account = "ben";
     const u3 = createU3(config);
     const contract = await u3.contract(account);
-    await contract.hi('ben', 30, 'It is a test', { authorization: [`ben@active`] });
+    await contract.hi("ben", 30, "It is a test", { authorization: [`ben@active`] });
   });
 
 
-  it('block', async () => {
+  it("block", async () => {
 
-    let account = 'ben';
+    let account = "ben";
     const u3 = createU3(config);
     const contract = await u3.contract(account);
-    const tx = await contract.hi('ben', 30, 'It is a test', { authorization: [`ben@active`] });
+    const tx = await contract.hi("ben", 30, "It is a test", { authorization: [`ben@active`] });
 
-    //wait util it was packed in a block
+    //wait max a minute util it was packed in a block
     let tx_trace = await u3.getTxByTxId(tx.transaction_id);
-    while (!tx_trace.irreversible) {
-      await U3Utils.test.wait(1000);
+    let time = 0;
+    let timer = setInterval(async () => {
+      time++;
+      if (time >= 60) {
+        clearInterval(timer);
+        return;
+      }
       tx_trace = await u3.getTxByTxId(tx.transaction_id);
       if (tx_trace.irreversible) {
-        console.log(tx);
-        break;
+        console.log("Confirmed success");
+        clearInterval(timer);
+      } else {
+        // eslint-disable-next-line
+        console.log("Waiting " + time + "s");
       }
-    }
+    }, 1000);
 
   });
 });
