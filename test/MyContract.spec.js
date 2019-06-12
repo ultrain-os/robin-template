de23fb7ba00dfa11b28ec03cc602100e05dba7ce
benyasin <rgwapkk520>
2019-06-12 14:10:45 +0800

    update test case

--- test/MyContract.spec.js
+++ test/MyContract.spec.js
@@ -1,5 +1,4 @@
-const U3Utils = require("u3-utils/src");
-const { createU3, format, listener } = require("u3.js/src");
+const { createU3, U3Utils } = require("u3.js");
 const config = require("../config");
 
 const chai = require("chai");
@@ -25,27 +24,26 @@ describe("Test cases", function() {
     let account = "ben";
     const u3 = createU3(config);
     const contract = await u3.contract(account);
-    const tx = await contract.hi("ben", 30, "It is a test", { authorization: [`ben@active`] });
+    const result = await contract.hi("ben", 30, "It is a test", { authorization: [`ben@active`] });
 
-    //wait max a minute util it was packed in a block
-    let tx_trace = await u3.getTxByTxId(tx.transaction_id);
-    let time = 0;
-    let timer = setInterval(async () => {
-      time++;
-      if (time >= 70) {
-        clearInterval(timer);
-        return;
-      }
-      tx_trace = await u3.getTxByTxId(tx.transaction_id);
-      if (tx_trace.irreversible) {
-        // eslint-disable-next-line
-        console.log("Confirmed success");
-        clearInterval(timer);
-      } else {
-        // eslint-disable-next-line
-        console.log("Waiting " + time + "s");
-      }
-    }, 1000);
 
+    // first check whether the transaction was failed
+    if (!result || result.processed.receipt.status !== "executed") {
+      console.log("the transaction was failed");
+      return;
+    }
+
+    // then check whether the transaction was irreversible when it was not expired
+    let timeout = new Date(result.transaction.transaction.expiration + "Z") - new Date();
+    let finalResult = false;
+    try {
+      await U3Utils.test.waitUntil(async () => {
+        let tx = await u3.getTxByTxId(result.transaction_id);
+        finalResult = tx && tx.irreversible;
+        if (finalResult) return true;
+      }, timeout, 1000);
+    } catch (e) {
+      console.log(finalResult);
+    }
   });
 });
